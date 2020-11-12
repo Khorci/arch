663e3b08f652a00d2e70441aa8bd5f8326b857ee
Matheus Vinicius <matheusviniciusdevs@gmail.com>
2020-11-12 20:55:53 -0300

    some edits

--- basic_config.sh
+++ basic_config.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+TZ="America/Sao_Paulo"
+HN="arch"
+
+ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
 hwclock --systo
 
 sed -i "s/^#pt_BR.UTF-8\ UTF-8/pt_BR.UTF-8\ UTF-8/" /etc/locale.gen
 sed -i "s/^#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/" /etc/locale.gen
 locale-gen
 
-echo -e "LANG=pt_BR.UTF-8\nLANGUAGE=pt_BR:pt:en" >> /etc/locale.conf
+echo -e "LANG=pt_BR.UTF-8\nLANGUAGE=pt_BR:pt:en\nLC_MESSAGES=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
-echo -e "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tarch.localdomain\tarch" >> /etc/hosts
+echo "$HN" >> /etc/hostname
+echo -e "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$HN.localdomain\t$HN" >> /etc/hosts
 
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
