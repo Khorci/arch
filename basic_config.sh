6a8a034edc02c8387550cc8395e5400e0f356891
Matheus Vinicius <matheusviniciusdevs@gmail.com>
2020-11-13 08:12:02 -0300

    add user

--- basic_config.sh
+++ basic_config.sh
@@ -2,9 +2,10 @@
 
 TZ="America/Sao_Paulo"
 HN="arch"
+USER="matheus"
 
 ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
-hwclock --systo
+hwclock --systo --utc
 
 sed -i "s/^#pt_BR.UTF-8\ UTF-8/pt_BR.UTF-8\ UTF-8/" /etc/locale.gen
 sed -i "s/^#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/" /etc/locale.gen
@@ -15,6 +16,14 @@ echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
 echo "$HN" >> /etc/hostname
 echo -e "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$HN.localdomain\t$HN" >> /etc/hosts
 
+grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
+grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+
+sed -i "s/#\ %wheel\ ALL=(ALL)\ ALL/%wheel\ ALL=(ALL)\ ALL/" /etc/sudoers
+
+useradd -mg wheel -G storage,power,audio,video,optical -s /bin/zsh $USER
+chown -R $USER:wheel /home/$USER
+
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
 systemctl start NetworkManager
@@ -24,3 +33,4 @@ pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda &&
 mkinitcpio -P
 
 passwd
+passwd $USER
